04981e6eccd2213637dc39a1ff16d266079828f5
san64777 <san64777@gmail.com>
2023-10-09 12:40:54 +0530

    update

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.common.exceptions import NoSuchElementException, WebDriverException
-from bs4 import BeautifulSoup
-import time
+from selenium.common.exceptions import NoSuchElementException, WebDriverException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import logging
+import time
 
 logging.basicConfig(level=logging.INFO)
 
 def get_search_results_count(term, proxy, max_retries=3):
 	# Prepare the proxy
 	chrome_options = Options()
-	chrome_options.add_argument("--headless")
+	# chrome_options.add_argument("--headless")
 	chrome_options.add_argument("--log-level=3")  # Suppress logging
 	chrome_options.add_argument("--disable-logging")  # Suppress logging
+	chrome_options.add_argument('--ignore-certificate-errors')
 	
 	webdriver.DesiredCapabilities.CHROME['proxy'] = {
 		"httpProxy": proxy,
@@ -30,22 +30,36 @@ def get_search_results_count(term, proxy, max_retries=3):
 	while retries < max_retries:
 		try:
 			driver.get("https://branddb.wipo.int/en/quicksearch")
-			time.sleep(2)
-
-			search_box = driver.find_element(By.XPATH,'//*[contains(text(),"Search by brand name")]/following-sibling::input')
+			
+			search_box = WebDriverWait(driver, 10).until(
+				EC.presence_of_element_located((By.XPATH,'//*[contains(text(),"Search by brand name")]/following-sibling::input'))
+			)
 			search_box.send_keys(term)
-			search_button = driver.find_element(By.XPATH,'//button[contains(text(),"Search")]')
+			
+			search_button = WebDriverWait(driver, 10).until(
+				EC.element_to_be_clickable((By.XPATH,'//button[contains(text(),"Search")]'))
+			)
 			search_button.click()
-			time.sleep(2)
-				
-			soup = BeautifulSoup(driver.page_source, "html.parser")
-			results_text = soup.find("div", {"data-test-id": "resultsCount"}).text
+			try:
+				WebDriverWait(driver, 10).until(
+					lambda driver: driver.find_element(By.XPATH, '//div[@data-test-id="resultsCount"]').text.strip() not in ["", "Loading..."]
+				)
+				results_text = driver.find_element(By.XPATH, '//div[@data-test-id="resultsCount"]').text
+
+			except TimeoutException:
+				print("Timed out waiting for element to appear")
+				results_text = ""
+
+			except IndexError:
+				print("IndexError occurred")
+				results_text = ""
+
 			if "No results found!" in results_text:
 				print("No results found for the search term.")
 				return 0
-				
 			results_count = int(results_text.split('of')[1].strip().split(' ')[0])
 			return results_count
+			
 
 		except NoSuchElementException as e:
 			logging.error("Element not found, check your XPaths or element identifiers.")
@@ -68,8 +82,6 @@ def get_search_results_count(term, proxy, max_retries=3):
 	logging.error("Max retries reached. Exiting function.")
 	return None
 	
-	
-
 if __name__ == "__main__":
 	term = input("Enter the search term: ")
 	proxy = "194.102.39.101:12345:tbtbtb:mxVFyaOE"
